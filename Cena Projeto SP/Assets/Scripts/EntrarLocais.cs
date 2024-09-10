225e0babd869bc3d78facbd90154cb6baf32923c
CauaSilva28 <127631052+CauaSilva28@users.noreply.github.com>
2024-09-10 08:42:01 -0300

    update fase 5

---
+++ Cena Projeto SP/Assets/Scripts/EntrarLocais.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EntrarLocais : MonoBehaviour
+{
+    public Transform posicaoTeleporte;
+    public Text textoTeclas;
+
+    public string textoNasTeclas;
+
+    public GameObject player;
+
+    public GameObject telaTransicaoTeleporte;
+
+    private bool teleportar = false;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (teleportar)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                StartCoroutine(EntrarLocal());
+                teleportar = false;
+            }
+        }
+    }
+
+    IEnumerator EntrarLocal()
+    {
+        telaTransicaoTeleporte.SetActive(true);
+        telaTransicaoTeleporte.GetComponent<Animator>().SetInteger("transition", 1);
+
+        yield return new WaitForSeconds(1f);
+
+        player.GetComponent<MovimentoPerso>().enabled = false;
+        Vector3 posicaoPlayer = posicaoTeleporte.position;
+        player.GetComponent<Transform>().position = posicaoPlayer;
+
+        yield return new WaitForSeconds(1f);
+
+        player.GetComponent<MovimentoPerso>().enabled = true;
+
+        yield return new WaitForSeconds(1f);
+
+        telaTransicaoTeleporte.GetComponent<Animator>().SetInteger("transition", 2);
+
+        yield return new WaitForSeconds(2f);
+
+        telaTransicaoTeleporte.SetActive(true);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            textoTeclas.enabled = true;
+            textoTeclas.text = textoNasTeclas;
+            teleportar = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            textoTeclas.enabled = false;
+            textoTeclas.text = "";
+            teleportar = false;
+        }
+    }
+}
